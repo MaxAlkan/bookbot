07d493d7495af7b15cc4a578f3c5e748e9cd3667
MaxAlkan <max.b.garcia0@gmail.com>
2025-09-18 23:54:48 -0700

    Completed the book_report function and the list/sort function. Required help from boots at the end.

--- main.py
+++ main.py
@@ -1,19 +1,34 @@
-from stats import word_count, character_count
+from stats import (
+    word_count,
+    character_count,
+    chars_dict_to_sorted_list,
+)
+
+def main():
+    book_path = "./books/frankenstein.txt"
+    text = get_book_text(book_path)
+    num_words = word_count(text)
+    chars_dict = character_count(text)
+    chars_sorted_list = chars_dict_to_sorted_list(chars_dict)
+    book_report(book_path, num_words, chars_sorted_list)
+
 
 def get_book_text(file_path):
     with open(file_path) as f:
         file_contents = f.read()
     return file_contents
 
-def book_report(input_text):
+def book_report(book_path, num_words, chars_sorted_list):
     print("============ BOOKBOT ============")
-    print(f"Analyzing book found at {input_text}")
+    print(f"Analyzing book found at {book_path}...")
     print("----------- Word Count ----------")
-    print(f"Found {word_count(get_book_text(input_text))} total words")
+    print(f"Found {num_words} total words")
     print("--------- Character Count -------")
-    print(character_count(get_book_text(input_text)))
-    
-def main():
-    book_report("./books/frankenstein.txt")
+    for item in chars_sorted_list:
+        if not item["char"].isalpha():
+            continue
+        print(f"{item['char']}: {item['num']}")
+    print("============= END ===============")
+
 
 main()
--- stats.py
+++ stats.py
@@ -10,3 +10,13 @@ def  character_count(input_text):
         else:
             character_dict[input.lower()] = 1        
     return character_dict
+
+def sort_on(d):
+    return d["num"]
+
+def chars_dict_to_sorted_list(character_count_dict):
+    sorted_list = []
+    for ch in character_count_dict:
+        sorted_list.append({"char": ch, "num" : character_count_dict[ch]})
+    sorted_list.sort(reverse=True, key=sort_on)
+    return sorted_list
